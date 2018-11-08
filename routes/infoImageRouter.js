0655c75808a2f05385a1eb6b88da845476fb550a
Amaury-Tobias@CSI <amaury.tobiasqr+csi@gmail.com>
2018-11-08 13:06:45 -0600

    upload info pictures

--- routes/infoImageRouter.js
+++ routes/infoImageRouter.js
@@ -8,25 +8,30 @@ const passport = require('passport');
 
 const upload = multer({ dest: 'uploads/' });
 
-router.post('/info/picture', passport.authenticate('jwt', { session: false }), upload.single('img'), function (req, res) {
-    const strId = req.body.strId;
+router.post('/info/picture',
+    passport.authenticate('jwt', { session: false }),
+    upload.single('sInfo'), function (req, res, next) {
 
-    InfoModel.findOneAndDelete({ strId: strId })
-        .then(result => {
-            var newPic = new InfoModel();
-            if (result != null) {
-                newPic._id = result._id;
-            }
-            newPic.img.contentType = req.file.mimetype;
-            newPic.img.data = fs.readFileSync(req.file.path);
-            newPic.strId = strId;
-            newPic.save();
-            return res.json(newPic._id);
-        })
-        .catch(err => {
-            return res.json(err);
-        });
-});
+        const strId = req.body.option;
+
+        InfoModel.findOneAndDelete({ strId: strId })
+            .then(result => {
+                var newPic = new InfoModel();
+                if (result != null) {
+                    newPic._id = result._id;
+                }
+                
+                newPic.img.contentType = req.file.mimetype;
+                newPic.img.data = fs.readFileSync(req.file.path);
+                newPic.strId = strId;
+                newPic.save();
+                return res.status(200).redirect('/panel');
+            })
+            .catch(err => {
+                console.log(err);
+                return next(err)
+            });
+    });
 
 router.get('/info/picture/:id', async function (req, res, next) {
     let id = req.params.id;
