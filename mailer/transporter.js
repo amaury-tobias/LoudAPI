281fe003706168f33577b192c414610861f100b7
Amaury-Tobias@CSI <amaury.tobiasqr+csi@gmail.com>
2018-11-08 11:58:23 -0600

    password update

--- mailer/transporter.js
+++ mailer/transporter.js
@@ -4,7 +4,7 @@ var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'amaury.tobiasqr@gmail.com',
-        pass: 'awpbseerwbzjxjgt'
+        pass: 'abzmlxvsqpyettjk'
     }
 });
 
